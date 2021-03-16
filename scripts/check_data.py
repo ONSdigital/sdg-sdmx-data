d8c1b8a7940a2e9a24fa8f4aa37170bf6be5a10d
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-03-16 15:03:01 +0000

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,7 +1,20 @@
 from sdg.open_sdg import open_sdg_check
+import pandas as pd
+
+sdmx_mapping=pd.read_csv('sdmx_mapping.csv')
+
+def alter_data(data):
+  for col in data.columns:
+    if col not in ["Year", "Value"]:
+        for i in data.index:
+            data.at[i, col]=sdmx_mapping['OpenSDG_itemval'].loc[sdmx_mapping['CSV_itemval']==data.at[i, col]].item()
+        newcol=sdmx_mapping['OpenSDG_colval'].loc[sdmx_mapping['CSV_colval']==col].item()
+        data.rename(columns={col:newcol}, inplace=True)
+
+  return data
 
 # Validate the indicators.
-validation_successful = open_sdg_check(config='config_data.yml')
+validation_successful = open_sdg_check(config='config_data.yml', alter_data=alter_data)
 
 # If everything was valid, perform the build.
 if not validation_successful:
