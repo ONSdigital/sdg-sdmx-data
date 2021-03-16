539b21339766c4f215c240ac53869730e4af2550
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-03-16 15:01:45 +0000

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,16 @@
 from sdg.open_sdg import open_sdg_build
+import pandas as pd
 
-open_sdg_build(config='config_data.yml')
+sdmx_mapping<-pd.read_csv('sdmx_mapping.csv')
+
+def alter_data(data):
+  for col in data.columns:
+    if col not in ["Year", "Value"]:
+        for i in data.index:
+            data.at[i, col]=sdmx_mapping['OpenSDG_itemval'].loc[sdmx_mapping['CSV_itemval']==data.at[i, col]].item()
+        newcol=sdmx_mapping['OpenSDG_colval'].loc[sdmx_mapping['CSV_colval']==col].item()
+        data.rename(columns={col:newcol}, inplace=True)
+
+  return data
+
+open_sdg_build(config='config_data.yml', alter_data=alter_data)
